3fa668f988259238a6fb36e86d638a60b7772432
dieciocho <113624568+dieciocho@users.noreply.github.com>
2022-11-23 04:40:46 -0600

    Update command.py

--- command.py
+++ command.py
@@ -1,1 +1,6 @@
-print("frank")
+import pandas as pd
+data = pd.read_csv("city_temperature.csv")
+data["HighTemp"] = data.groupby(by="Region")["AvgTemperature"].transform(max)
+result = data[data["HighTemperature"] == data["HighTemp"]]
+result = result.drop("HighTemp", axis="columns")
+result.to_csv("city_maxtemp.csv", index=False)
